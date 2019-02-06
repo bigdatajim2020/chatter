9d1ffa547476008b82582b2cb67f947fb3870fa9
Willaim <1340440725fhs@gmail.com>
2019-02-06 17:06:42 +0800

    User data, unfinished

---
+++ data/user.go
@@ -0,0 +1,22 @@
+package data
+
+import "time"
+
+// A User represents a registered user.
+type User struct {
+	ID        int
+	UUID      string
+	Name      string
+	Email     string
+	Password  string
+	CreatedAt time.Time
+}
+
+// A Session represents an authenticated user session.
+type Session struct {
+	ID        int
+	UUID      string // randomly generated unique ID as a cookie value.
+	Email     string // User registered email.
+	UserID    int    // ID of user table row in database.
+	CreatedAt time.Time
+}
