0514ca726a144a7f9a88371305c45103736c0f91
Willaim <1340440725fhs@gmail.com>
2019-02-07 16:19:17 +0800

    thread post

--- datastore/thread.go
+++ datastore/thread.go
@@ -10,3 +10,13 @@ type Thread struct {
 	UserID    int
 	CreatedAt time.Time
 }
+
+// A Post represents a post (a message added by a forum user) within a thread.
+type Post struct {
+	ID        int
+	UUID      string
+	Body      string
+	UserID    int
+	ThreadID  int
+	CreatedAt time.Time
+}
