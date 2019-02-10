335f343e7035334b39a5e70f930181146d3cdbb6
Willaim <1340440725fhs@gmail.com>
2019-02-10 21:18:16 +0800

    thread: threads

--- datastore/thread.go
+++ datastore/thread.go
@@ -1,6 +1,8 @@
 package datastore
 
-import "time"
+import (
+	"time"
+)
 
 // A Thread represents a forum thread (a conversation among forum users).
 type Thread struct {
@@ -19,4 +21,33 @@ type Post struct {
 	UserID    int
 	ThreadID  int
 	CreatedAt time.Time
+}
+
+// Threads extracts all threads in the database foe the index handler.
+func Threads() (threads []Thread, err error) {
+	q := `
+	select
+		id, uuid, topic, user_id, created_at
+	from
+		threads
+	order by
+		created_at
+	desc`
+	rows, err := Db.Query(q)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var t Thread
+		if err = rows.Scan(&t.ID, &t.UUID, &t.Topic, &t.UserID, &t.CreatedAt); err != nil {
+			return
+		}
+		threads = append(threads, t)
+	}
+	if err = rows.Err(); err != nil {
+		return
+	}
+	return
 }
