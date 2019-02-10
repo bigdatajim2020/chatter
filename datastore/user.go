f5fadd2d58cc51b4c5b0d64cd3e5d19a05828a31
Willaim <1340440725fhs@gmail.com>
2019-02-10 21:59:24 +0800

    user: UserByEmail

--- datastore/user.go
+++ datastore/user.go
@@ -20,3 +20,17 @@ type Session struct {
 	UserID    int    // ID of user table row in database.
 	CreatedAt time.Time
 }
+
+// UserByEmail gets a single user by the given email.
+func UserByEmail(email string) (u User, err error) {
+	q := `
+		select
+			id, uuid, name, email, password, created_at
+		from
+			users
+		where
+			email = $1
+	`
+	err = Db.QueryRow(q, email).Scan(&u.ID, &u.UUID, &u.Name, &u.Email, &u.Password, &u.CreatedAt)
+	return
+}
