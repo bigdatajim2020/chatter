35a406c395503e3376524d297c21dc4dc71da976
Willaim <1340440725fhs@gmail.com>
2019-02-14 20:46:54 +0800

    added https support

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
@@ -21,5 +23,22 @@ func main() {
 	http.HandleFunc("/thread/post", postThreadHandler)
 	http.HandleFunc("/thread/read", readThreadHandler)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+	// create the autocert.Manager with domains and path to the cache.
+	m := &autocert.Manager{
+		Cache:      autocert.DirCache("secret-dir"),
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("hostname"), // must not be localhost
+	}
+	s := http.Server{
+		Addr:      ":https",
+		TLSConfig: m.TLSConfig(),
+	}
+	go func() {
+		// server HTTP, which will redirect automatically to HTTPS.
+		h := m.HTTPHandler(nil)
+		log.Fatal(http.ListenAndServe(":http", h))
+	}()
+	// serve HTTPS
+	log.Fatal(s.ListenAndServeTLS("", ""))
 }
