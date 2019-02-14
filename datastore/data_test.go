c1fa488d16188133654bede68addc43ed29b35e1
Willaim <1340440725fhs@gmail.com>
2019-02-14 15:17:23 +0800

    datastore/test: added user tests

---
+++ datastore/data_test.go
@@ -0,0 +1,15 @@
+package datastore
+
+// Data for testing.
+var users = []User{
+	{
+		Name:     "Peter Jones",
+		Email:    "peter@gmail.com",
+		Password: "peter_pass",
+	},
+	{
+		Name:     "John Smith",
+		Email:    "john@gmail.com",
+		Password: "john_pass",
+	},
+}
---
+++ datastore/user_test.go
@@ -0,0 +1,184 @@
+package datastore
+
+import (
+	"database/sql"
+	"testing"
+)
+
+func TestUserNew(t *testing.T) {
+	defer DeleteAll("users")
+	if err := users[0].New(); err != nil {
+		t.Errorf("create new user: %v", err)
+	}
+	if users[0].ID == 0 {
+		t.Error("No id or created_at in user")
+	}
+	u, err := UserByEmail(users[0].Email)
+	if err != nil {
+		t.Errorf("failed creating new user: %v", err)
+	}
+	if users[0].Email != u.Email {
+		t.Errorf("user email: %q, want: %q", u.Email, users[0].Email)
+	}
+}
+
+func Test_UserDelete(t *testing.T) {
+	defer DeleteAll("users")
+	if err := users[0].New(); err != nil {
+		t.Error(err, "Cannot create user.")
+	}
+	if err := users[0].Delete(); err != nil {
+		t.Error(err, "- Cannot delete user")
+	}
+	_, err := UserByEmail(users[0].Email)
+	if err != sql.ErrNoRows {
+		t.Error(err, "- User not deleted.")
+	}
+}
+
+func Test_UserUpdate(t *testing.T) {
+	defer DeleteAll("users")
+	if err := users[0].New(); err != nil {
+		t.Error(err, "Cannot create user.")
+	}
+	users[0].Name = "Random User"
+	if err := users[0].Update(); err != nil {
+		t.Error(err, "- Cannot update user")
+	}
+	u, err := UserByEmail(users[0].Email)
+	if err != nil {
+		t.Error(err, "- Cannot get user")
+	}
+	if u.Name != "Random User" {
+		t.Error(err, "- User not updated")
+	}
+}
+
+func Test_UserByUUID(t *testing.T) {
+	defer DeleteAll("users")
+	if err := users[0].New(); err != nil {
+		t.Error(err, "Cannot create user.")
+	}
+	u, err := UserByUUID(users[0].UUID)
+	if err != nil {
+		t.Error(err, "User not created.")
+	}
+	if users[0].Email != u.Email {
+		t.Errorf("User retrieved is not the same as the one created.")
+	}
+}
+
+func Test_Users(t *testing.T) {
+	defer DeleteAll("users")
+	for _, user := range users {
+		if err := user.New(); err != nil {
+			t.Error(err, "Cannot create user.")
+		}
+	}
+	u, err := Users()
+	if err != nil {
+		t.Error(err, "Cannot retrieve users.")
+	}
+	if len(u) != 2 {
+		t.Error(err, "Wrong number of users retrieved")
+	}
+	if u[0].Email != users[0].Email {
+		t.Error(u[0], users[0], "Wrong user retrieved")
+	}
+}
+
+func Test_NewSession(t *testing.T) {
+	defer DeleteAll("users")
+	if err := users[0].New(); err != nil {
+		t.Error(err, "Cannot create user.")
+	}
+	session, err := users[0].NewSession()
+	if err != nil {
+		t.Error(err, "Cannot create session")
+	}
+	if session.UserID != users[0].ID {
+		t.Error("User not linked with session")
+	}
+}
+
+func Test_GetSession(t *testing.T) {
+	defer DeleteAll("users")
+	if err := users[0].New(); err != nil {
+		t.Error(err, "Cannot create user.")
+	}
+	session, err := users[0].NewSession()
+	if err != nil {
+		t.Error(err, "Cannot create session")
+	}
+
+	s, err := users[0].Session()
+	if err != nil {
+		t.Error(err, "Cannot get session")
+	}
+	if s.ID == 0 {
+		t.Error("No session retrieved")
+	}
+	if s.ID != session.ID {
+		t.Error("Different session retrieved")
+	}
+}
+
+func Test_checkValidSession(t *testing.T) {
+	defer DeleteAll("users")
+	if err := users[0].New(); err != nil {
+		t.Error(err, "Cannot create user.")
+	}
+	session, err := users[0].NewSession()
+	if err != nil {
+		t.Error(err, "Cannot create session")
+	}
+
+	uuid := session.UUID
+
+	s := Session{UUID: uuid}
+	valid, err := s.Check()
+	if err != nil {
+		t.Error(err, "Cannot check session")
+	}
+	if valid != true {
+		t.Error(err, "Session is not valid")
+	}
+
+}
+
+func Test_checkInvalidSession(t *testing.T) {
+	defer DeleteAll("users")
+	s := Session{UUID: "123"}
+	valid, err := s.Check()
+	if err == nil {
+		t.Error(err, "Session is not valid but is validated")
+	}
+	if valid == true {
+		t.Error(err, "Session is valid")
+	}
+
+}
+
+func Test_DeleteSession(t *testing.T) {
+	defer DeleteAll("users")
+	if err := users[0].New(); err != nil {
+		t.Error(err, "Cannot create user.")
+	}
+	session, err := users[0].NewSession()
+	if err != nil {
+		t.Error(err, "Cannot create session")
+	}
+
+	err = session.DeleteByUUID()
+	if err != nil {
+		t.Error(err, "Cannot delete session")
+	}
+	s := Session{UUID: session.UUID}
+	valid, err := s.Check()
+	if err == nil {
+		t.Error(err, "Session is valid even though deleted")
+	}
+	if valid == true {
+		t.Error(err, "Session is not deleted")
+	}
+}
